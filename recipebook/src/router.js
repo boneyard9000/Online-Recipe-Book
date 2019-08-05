af3b82ca3e2d8f4e4eededb7e71dd9fffb42fa1d
Collin Thompson <collinofthethompsons@gmail.com>
2019-08-05 12:23:46 -0400

    inital set up of homepage

--- recipebook/src/router.js
+++ recipebook/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import HomePage from './views/HomePage.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path:'/HomePage',
+      name: 'HomePage',
+      component: HomePage
     },
     {
       path: '/about',
