81a97fa786a744307627e025423cfac4ca81e598
Chris Vanderhorst <christophervanderhorst19@gmail.com>
2019-08-05 12:19:26 -0400

    Changes to LandingPage for initial setup

--- recipebook/src/router.js
+++ recipebook/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import LandingPage from './views/LandingPage.vue'
 
 Vue.use(Router)
 
@@ -14,6 +15,11 @@ export default new Router({
       component: Home
     },
     {
+      path:'/landingPage',
+      name: 'landingPage',
+      component: LandingPage
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -21,5 +27,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+   
   ]
 })
