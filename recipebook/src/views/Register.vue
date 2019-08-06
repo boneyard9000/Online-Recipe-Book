2a0e7c5b7d70c6f90ba470ee54c907a80c3a4460
Spencer Schwier <jsschwier@gmail.com>
2019-08-06 14:29:39 -0400

    added bootstrap styling to register mainly for size and placement

--- recipebook/src/views/Register.vue
+++ recipebook/src/views/Register.vue
@@ -1,89 +1,104 @@
 <template>
-  <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        There were problems registering this user.
-      </div>
-      <label for="firstName" class="sr-only">First Name: </label>
-      <input
-        type="text"
-        id="firstname"
-        class="form-control"
-        placeholder="First Name:"
-        v-model="user.firstName"
-        required
-        autofocus
-      />
-      <br/>
-      <br/>
-      <label for="lastName" class="sr-only">Last Name: </label>
-        <input
-        type="text"
-        id="lastname"
-        class="form-control"
-        placeholder="Last Name:"
-        v-model="user.lastName"
-        required
-        autofocus
-      />
-       <br/>
-      <br/>
-      <label for="password" class="sr-only">Password: </label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <br/>
-      <br/>
-      <label for="confirmPassword" class="sr-only">Confrim Password: </label>
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-       <br/>
-       <br/>
-        <label for="email" class="sr-only">Email: </label>
-        <input
-        type="text"
-        id="email"
-        class="form-control"
-        placeholder="email"
-        v-model="user.email"
-        required
-        autofocus
-      />
-      <br/>
-      <br/>
-        <label for="phone" class="sr-only">Phone Number: </label>
-        <input
-        type="text"
-        id="phone"
-        class="form-control"
-        placeholder="Phone Number"
-        v-model="user.phone"
-        required
-        autofocus
-      />
-      <br/>
-      <br/>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
-      <br/>
-      <br/>
-      <router-link :to="{ name: 'login' }">
-        Have an account?
-      </router-link>
-    </form>
+  <div id="register" class="container">
+    <b-form cols="4" class="form-register" @submit.prevent="register">
+
+          <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+          <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+            There were problems registering this user.
+          </div>
+
+          <b-form-group id="input-group-1">
+          <b-form-input
+            type="text"
+            id="firstname"
+            class="form-control"
+            placeholder="First Name:"
+            v-model="user.firstName"
+            required
+            autofocus
+          />
+          </b-form-group>
+
+          <br/>
+
+          <b-form-group id="input-group-2">
+          <b-form-input
+            type="text"
+            id="lastname"
+            class="form-control"
+            placeholder="Last Name:"
+            v-model="user.lastName"
+            required
+            autofocus
+          />
+          </b-form-group>
+          
+          <br/>
+
+          <b-form-group id="input-group-3">
+          <b-form-input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+          </b-form-group>
+          
+          <br/>
+
+          <b-form-group id="input-group-4">
+          <b-form-input
+            type="password"
+            id="confirmPassword"
+            class="form-control"
+            placeholder="Confirm Password"
+            v-model="user.confirmPassword"
+            required
+          />
+          </b-form-group>
+          
+          <br/>
+
+            <b-form-group id="input-group-5">
+          <b-form-input
+            type="text"
+            id="email"
+            class="form-control"
+            placeholder="email"
+            v-model="user.email"
+            required
+            autofocus
+          />
+            </b-form-group>
+          
+          <br/>
+
+            <b-form-group id="input-group-6">
+          <b-form-input
+            type="text"
+            id="phone"
+            class="form-control"
+            placeholder="Phone Number"
+            v-model="user.phone"
+            required
+            autofocus
+          />
+            </b-form-group>
+        
+          <br/>
+
+          <b-button pill variant="success">
+            Create Account
+          </b-button>
+          
+          <br/>
+
+          <router-link :to="{ name: 'login' }">
+            Have an account?
+          </router-link>
+    </b-form>
   </div>
 </template>
 
@@ -135,4 +150,8 @@ export default {
 </script>
 
 <style>
+.form-register {
+  max-width: 50%;
+  margin: auto;
+}
 </style>
