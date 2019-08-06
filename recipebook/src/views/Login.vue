7ff29504666867a061d91eeb781124910007c861
Spencer Schwier <jsschwier@gmail.com>
2019-08-06 15:19:14 -0400

    more styling to make both login and register uniform

--- recipebook/src/views/Login.vue
+++ recipebook/src/views/Login.vue
@@ -1,42 +1,46 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
+  <div id="login" class="container">
+    <b-form cols="4" class="form-signin" @submit.prevent="login">
+
       <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
       <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
+
       <div class="alert alert-success" role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
-       <label for="email" class="sr-only">Email: </label>
-        <input
-        type="text"
-        id="email"
-        class="form-control"
-        placeholder="email"
-        v-model="user.email"
-        required
-        autofocus
-      />
+
+       <b-form-group id="input-group-1">
+          <b-form-input
+            type="text"
+            id="email"
+            class="form-control"
+            placeholder="email"
+            v-model="user.email"
+            required
+            autofocus
+          />
+       </b-form-group>
+      
+      <b-form-group id="input-group-2">
+          <b-form-input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+      </b-form-group>
+
+      <b-button pill variant="success">Sign in</b-button>
+
       <br/>
-      <br/>
-      <label for="password" class="sr-only">Password: </label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <br/>
-      <br/>
-      <button type="submit">Sign in</button>
-      <br/>
-      <br/>
+      
       <router-link :to="{ name: 'Register' }">Need an account?</router-link>
       
-    </form>
+    </b-form>
   </div>
 </template>
 
@@ -88,5 +92,8 @@ export default {
 </script>
 
 <style scoped>
-
+.form-signin {
+  max-width: 50%;
+  margin: auto;
+}
 </style>
