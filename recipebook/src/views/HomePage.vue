df75c3a7bc145a9be9023518db9578efc74276c0
Collin Thompson <collinofthethompsons@gmail.com>
2019-08-06 12:25:00 -0400

    created recipe details

--- recipebook/src/views/HomePage.vue
+++ recipebook/src/views/HomePage.vue
@@ -46,18 +46,18 @@ export default {
     data() {
         return {
             recipes: [
-                {name: 'Chicken Cheesecake', picSrc: require('../assets/stock.jpg'), description: 'sounds weird...is weird', steps: ['get a chicken',
+                {id: 1, name: 'Chicken Cheesecake', picSrc: require('../assets/stock.jpg'), description: 'sounds weird...is weird', steps: ['get a chicken',
                 'kill and butcher, its easier if you do it quick', 'get a cheesecake', 'put chicken in to cheesecake', 'bake at 350 until golden brown']},
-                {name: 'Hot Dog Margarita', picSrc: require('../assets/stock.jpg'), description: 'technically edible', steps: ['purchase pack of 8 hot dogs minimum quality',
+                {id: 2, name: 'Hot Dog Margarita', picSrc: require('../assets/stock.jpg'), description: 'technically edible', steps: ['purchase pack of 8 hot dogs minimum quality',
                 'beat to pulp with mortar and pestal', 'cover in tequila', 'let cure in sun for half day']},
-                {name: 'Literal Dirt', picSrc: require('../assets/stock.jpg'), description: 'its literally just dirt', steps: ['go outside', 'find some dirt',
+                {id: 3, name: 'Literal Dirt', picSrc: require('../assets/stock.jpg'), description: 'its literally just dirt', steps: ['go outside', 'find some dirt',
                 'its not very hard to see where it goes from here', 'put said dirt in bowl of your choosing', 'dump out said dirt', 
                 'repeat process until entire yard has been turned', 'wait 6-8 months for delicious veggies to grow', 'eat that, not the dirt.  Its better']},
-                {name: 'Oil Change', picSrc: require('../assets/stock.jpg'), description: 'this is not a recipe.  This is an oil change.  how did this get here',
+                {id: 4, name: 'Oil Change', picSrc: require('../assets/stock.jpg'), description: 'this is not a recipe.  This is an oil change.  how did this get here',
                 steps: ['let car sit at least 15 minutes to cool', 'jack up car and put on jack stands', 'open oil plug and drain in to drain pan', 'replace oil filter',
                 'refill your oil', 'replace drain plug.  also change drain plug gasket if necessary', 'put car back on to ground', 'fill the oil and check level once settled',
                 'Congratulations, you jsut changed you oil.  I bet youre hungry now...']},
-                {name: 'Unboiled Water', picSrc: require('../assets/stock.jpg'), description: 'tried of burnt water?  We can fix that', steps: ['Fill large pot with water', 
+                {id: 5, name: 'Unboiled Water', picSrc: require('../assets/stock.jpg'), description: 'tried of burnt water?  We can fix that', steps: ['Fill large pot with water', 
                 'put on high an let reach a rolling boil', 'remove pot from flame and take it outside', 'throw that nastiness out', 'refill pot with fresh, unboiled wter from the tap.  the less boiled the better',
                 'You have now learned how to make unboiled water.', 'profit']}
             ]
---
+++ recipebook/src/views/RecipeDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="recipe-details">
+        this is recipe details page
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RecipeDetails'
+
+}
+</script>
+
+<style>
+
+</style>
