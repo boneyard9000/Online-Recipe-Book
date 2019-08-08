0032c9ca7eaf9c66f75b8a3e09442075fc480f28
Spencer Schwier <jsschwier@gmail.com>
2019-08-08 17:39:09 -0400

    formatting style on details

--- recipebook/frontend/src/views/RecipeDetails.vue
+++ recipebook/frontend/src/views/RecipeDetails.vue
@@ -1,23 +1,37 @@
 <template>
+<<<<<<< Updated upstream
     <div id="recipe-details">
         <h2>{{currentRecipe.recipeName}}</h2>
         <h3>{{currentRecipe.category}}</h3>
         <div>Cook Time: {{currentRecipe.cookMins}}</div>
         <!-- <div><button>➕</button> Add to Favorites</div> -->
         <img src="../assets/stock.jpg">
+=======
+    <div id="recipe-details" class="container my-container">
+        <br><br><br><br>
+>>>>>>> Stashed changes
 
-        <label>Ingredients</label>
-         <ul v-for="item in ingredientsArray" :key="item">
-            <li>{{item}}</li>
-        </ul>
+        <div class="row my-row">
+            <div class="col-6 my-column">
+                <img style="max-width:550px;" src="../assets/stock.jpg">
+            </div>
+            <div class="col-6 my-column-2">
+                 <h2>{{currentRecipe.recipeName}}<br>
+                        {{currentRecipe.category}}</h2>
+             <div>Cook Time: {{currentRecipe.cookMins}}</div>
+
+            <label>Ingredients</label>
+            <ul v-for="item in ingredientsArray" :key="item">
+                <li>{{item}}</li>
+            </ul>
 
 
-        <label>Directions</label>
-        <ul v-for="step in directionsArray" :key="step">
-            <li>{{step}}</li>
-        </ul>
-
-
+            <label>Directions</label>
+            <ul v-for="step in directionsArray" :key="step">
+                <li>{{step}}</li>
+            </ul>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -82,10 +96,26 @@ export default {
 ul {
     background-color: rgb(175, 240, 145);
 }
+<<<<<<< Updated upstream
 h2 {
     font-size: 4em;
 }
 h3 {
     font-size: 3em;
+=======
+.my-row {
+    height: 100%;
+    width: 100%;
+}
+.my-column-2 {
+    justify-items: start;
+    padding: 2em;
+}
+.my-container {
+    
+    height: 100%;
+    margin: auto;
+    padding: 1em;
+>>>>>>> Stashed changes
 }
 </style>
