36369e7cc7f43a93338d21f88c4471392379ab01
Chris Vanderhorst <christophervanderhorst19@gmail.com>
2019-08-15 08:54:49 -0400

    All Recipes has been formatted

--- recipebook/frontend/src/views/AllRecipes.vue
+++ recipebook/frontend/src/views/AllRecipes.vue
@@ -1,23 +1,24 @@
 <template>
 <div class="text-color">
- <div class="extra-recipes-container">
-          <div class="container recipes" v-for="recipe in testRecipes" :key="recipe.recipeName">
-             <div class="recipe-left-side">
-
-            <div class="recipes-name">
-              {{recipe.recipeName}}
-            </div>
-            <router-link :to="{name: 'RecipeDetails', params: {id: recipe.recipeId}}">
-              <img  class="personal-recipe-pic" :src="recipe.recipePic" />
-            </router-link>
-          </div>
-            <div class="recipe-right-side">
-            <div class="personal-category">{{recipe.category}}</div>
-            <div class="personal-description">{{recipe.description}}</div>
-            <div class="personal-cook-time">Cook Time: {{recipe.cookMins}}</div>
-            </div>
-          </div>
+  <!-- The main-page div includes the nav bar -->
+  <div id="main-page" style="text-align: center;">
+    <div class="container" style="background: lightgray;">
+      <div class="box" v-for="recipe in testRecipes" :key="recipe.recipeName" style="float:left; margin: 10px;"> 
+        <div class="imgBox">
+          <img :src="recipe.recipePic" style="width: 100%; height: 250px;">
         </div>
+        <div class="content">
+        <h3>{{ recipe.recipeName }}</h3>
+        <p>Category: {{ recipe.category }}<br>
+          <br>Description: {{ recipe.description }}<br>
+          <br>Cook Time: {{ recipe.cookMins }}<br>
+          <br>For further information such as recipe ingredients and directions click on the button below<br>
+          <br><b-button :to="{name: 'RecipeDetails', params: {id: recipe.recipeId}}">Visit Recipe Page</b-button><br>
+        </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -76,20 +77,100 @@ export default {
 
 <style scoped>
 
-.personal-recipe-pic{
-  height: 30%;
-  width: 30%;
-  margin-left: 1%;
-  border: 4px solid white;
-  border-radius: 20px;
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  
 }
 
-.text-color{
-    color: white;
-    display: flex;
-    padding: 30px;
-
-
+.container {
+  width: 1000px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
+
+.box {
+  position: relative;
+  width: 300px;
+  height: 400px;
+  background: black;
+  box-shadow: 0 30px 30px rgba(0,0,0,.5);
+}
+
+.box .imgBox {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.box .imgBox img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  transition: 0.5s;
+}
+
+.box:hover .imgBox img {
+  opacity: 0;
+}
+
+.box .content {
+  position: absolute;
+  bottom: 20px;
+  left: 10%;
+  width: 80%;
+  height: 60px;
+  background: #fff;
+  transition: 0.5s;
+  overflow: hidden;
+  padding: 15px;
+  box-sizing: border-box;
+}
+
+.box:hover .content {
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  left: 0;
+}
+
+.box .content h3 {
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+}
+
+.box .content p {
+  margin: 10px 0 0;
+  padding: 0;
+  opacity: 0;
+  line-height: 1.2em;
+  transition: 0.5s;
+  text-align: justify;
+}
+
+.box:hover .content p {
+  opacity: 1;
+  transition-delay: 0.3s
+}
+
+
+
+
+
+
+
+
+
 
 </style>
